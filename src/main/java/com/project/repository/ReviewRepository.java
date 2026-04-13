69601513b9fad67f4a57b61342f3f6fae9cacfff
Yash Advani <54671322+yash976@users.noreply.github.com>
2026-04-13 16:41:10 +0530

    Create ReviewRepository.java

---
+++ src/main/java/com/project/repository/ReviewRepository.java
@@ -0,0 +1,9 @@
+package com.project.repository;
+
+import com.project.model.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+    List<Review> findByMovieId(Long movieId);
+}
