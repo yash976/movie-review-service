12d5fbd032659cba941f35fbea62bf0aba8e8250
Yash Advani <54671322+yash976@users.noreply.github.com>
2026-04-13 16:44:04 +0530

    Update MovieController.java

--- src/main/java/com/project/controller/MovieController.java
+++ src/main/java/com/project/controller/MovieController.java
@@ -1,4 +1,32 @@
 package com.project.controller;
 
+import com.project.model.Movie;
+import com.project.service.MovieService;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/movies")
 public class MovieController {
+
+    private final MovieService movieService;
+
+    public MovieController(MovieService movieService) {
+        this.movieService = movieService;
+    }
+
+    @PostMapping
+    public Movie create(@RequestBody Movie movie) {
+        return movieService.createMovie(movie);
+    }
+
+    @GetMapping
+    public List<Movie> getAll() {
+        return movieService.getAllMovies();
+    }
+
+    @GetMapping("/{id}")
+    public Movie getById(@PathVariable Long id) {
+        return movieService.getMovie(id);
+    }
 }
